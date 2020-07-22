9b73cde74a07cd484d37be28c64f125cd94df85e
Jan <jan.honsbrok@gmail.com>
2020-07-22 20:56:41 +0200

    add SubDivUI

---
+++ arnold_subdiv_manager/SubDivUI.py
@@ -0,0 +1,33 @@
+from arnold_subdiv_manager.subdiv_manager import SubDivManager
+from Qt import QtWidgets
+
+
+class SubDivUI(QtWidgets.QWidget):
+    def __init__(self, subdiv_manager):
+        # type: (SubDivManager) -> None
+        super(SubDivUI, self).__init__()
+        self._subdiv_manager = subdiv_manager
+        self._setup_ui()
+
+    def _setup_ui(self):
+        # type: () -> None
+        self.setWindowTitle("Arnold SubDiv Manager")
+        self._layout = QtWidgets.QVBoxLayout()
+        self.setLayout(self._layout)
+        self._btn_apply_subdiv = QtWidgets.QPushButton("Active Subdivision")
+        self._btn_apply_subdiv.clicked.connect(
+            self._subdiv_manager.apply_subdiv_to_selection
+        )
+        self._layout.addWidget(self._btn_apply_subdiv)
+
+
+if __name__ == "__main__":
+    from mock import MagicMock
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+
+    ui = SubDivUI(MagicMock())
+    ui.show()
+
+    app.exec_()
---
+++ tests/test_subdiv_manager_ui.py
@@ -0,0 +1,16 @@
+from mock import MagicMock
+from Qt import QtCore
+from pytestqt import qtbot
+
+from arnold_subdiv_manager.SubDivUI import SubDivUI
+
+
+def test_button_click_should_activate(qtbot):
+    # type: (qtbot) -> None
+    subdiv_manager = MagicMock()
+    ui = SubDivUI(subdiv_manager)
+    qtbot.addWidget(ui)
+
+    qtbot.mouseClick(ui._btn_apply_subdiv, QtCore.Qt.LeftButton)
+
+    subdiv_manager.apply_subdiv_to_selection.assert_called_once()
