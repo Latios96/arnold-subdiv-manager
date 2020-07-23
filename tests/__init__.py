356956037e69e466e9cd962a41606a400493162f
Jan <jan.honsbrok@gmail.com>
2020-07-23 20:39:10 +0200

    tests for MayaAbstractionImplementation

---
+++ tests/__init__.py
@@ -0,0 +1,22 @@
+import os
+
+import pytest
+
+
+def has_maya():
+    # type: () -> bool
+    """
+    Checks if PySioe or PySide2 is avaible
+    :return: True if avaible, False otherwise
+    """
+    try:
+        import maya.cmds as cmds
+
+        return True
+    except:
+        pass
+    return False
+
+
+maya_only = pytest.mark.skipif(not has_maya(), reason="requires Maya")
+skip_if_maya = pytest.mark.skipif(has_maya(), reason="can not run in Maya")
---
+++ tests/test_maya_abstraction_implementation.py
@@ -0,0 +1,34 @@
+from arnold_subdiv_manager.mesh import Mesh
+from tests import maya_only
+
+
+@maya_only
+def test_get_meshes_in_selection_should_only_return_meshes():
+    from arnold_subdiv_manager.maya_abstraction_implementation import MayaAbstractionImplementation
+    import pymel.core as pm
+
+    pm.newFile(f=True)
+    pm.camera()
+    pm.polyCube()
+    pm.select(['pCube1', 'camera1'])
+    maya_abstraction = MayaAbstractionImplementation()
+
+    meshes = maya_abstraction.get_meshes_in_selection()
+
+    assert meshes == [Mesh('|pCube1')]
+
+
+@maya_only
+def test_get_meshes_in_selection_should_handle_shapes_correctly():
+    from arnold_subdiv_manager.maya_abstraction_implementation import MayaAbstractionImplementation
+    import pymel.core as pm
+
+    pm.newFile(f=True)
+    pm.camera()
+    pm.polyCube()
+    pm.select(['|pCube1|pCubeShape1', '|pCube1'])
+    maya_abstraction = MayaAbstractionImplementation()
+
+    meshes = maya_abstraction.get_meshes_in_selection()
+
+    assert meshes == [Mesh('|pCube1|pCubeShape1'), Mesh('|pCube1')]
