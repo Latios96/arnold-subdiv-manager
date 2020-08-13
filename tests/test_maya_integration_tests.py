8fde751ac5984516d8a766766ce4c135d943a3ad
Jan <jan.honsbrok@gmail.com>
2020-08-13 11:52:21 +0200

    implement image comparison

--- tests/test_maya_integration_tests.py
+++ tests/test_maya_integration_tests.py
@@ -2,8 +2,6 @@ import os
 
 from arnold_subdiv_manager.subdiv_manager import SubDivManager
 from tests import maya_only
-import cv2
-import numpy as np
 
 
 def get_resource(name):
@@ -18,17 +16,32 @@ def render_to(file_path):
 
     pm.setAttr("defaultArnoldDriver.pre", file_path, type="string")
     arnoldRender(960, 540, True, True, "rendercam", " -layer defaultRenderLayer")
-    return file_path + "'_1.png"
+    return file_path + ".png"
 
 
 def assert_images_are_equal(image1, image2):
     # type: (str, str) -> None
+    assert os.path.exists(image1), "image1 does not exists!"
+    assert os.path.exists(image2), "image1 does not exists!"
 
-    x1 = cv2.imread("/Users/jan/workspace/arnold-subdiv-manager/tests/data/integration_test/after.png")
-    x2 = cv2.imread("/Users/jan/workspace/arnold-subdiv-manager/tests/data/integration_test/after.png")
+    from Qt import QtGui
 
-    diff = cv2.subtract(x1, x2)
-    assert not np.any(diff)
+    image_left = QtGui.QImage()
+    image_left.load(image1)
+    image_right = QtGui.QImage()
+    image_right.load(image2)
+
+    assert image_left.width() == image_right.width(), "Images are different in width!"
+    assert image_left.height() == image_right.height(), "Images are different in height!"
+
+    for x in range(image_left.width()):
+        for y in range(image_left.height()):
+            color_left = QtGui.QColor(image_left.pixel(x, y))
+            color_right = QtGui.QColor(image_right.pixel(x, y))
+            red_diff = (color_left.red() - color_right.red()) >= 50
+            green_diff = color_left.green() - color_right.green() >= 50
+            blue_diff = color_left.blue() - color_right.blue() >= 50
+            assert not red_diff and not green_diff and not blue_diff, "Pixel {}x{} does not match!".format(x,y)
 
 
 @maya_only
@@ -42,17 +55,18 @@ def test_integration_render_test(tmpdir):
     subdiv_manager = SubDivManager(maya_abstraction)
     pm.openFile(get_resource("cubes.ma"), f=True)
 
-    before_render = render_to(tmpdir.join("before"))
+    before_render = render_to(str(tmpdir.join("before")))
     assert_images_are_equal(get_resource("before.png"), before_render)
 
     pm.select("SUBDIV_CATCLARK")
     subdiv_manager.apply_subdiv_to_selection()
 
-    after_render = render_to(tmpdir.join("after"))
+    after_render = render_to(str(tmpdir.join("after")))
     assert_images_are_equal(get_resource("after.png"), after_render)
 
     pm.select("SUBDIV_CATCLARK")
     subdiv_manager.remove_subdiv_to_selection()
 
-    before_render = render_to(tmpdir.join("before"))
+    before_render = render_to(str(tmpdir.join("before")))
     assert_images_are_equal(get_resource("before.png"), before_render)
+
